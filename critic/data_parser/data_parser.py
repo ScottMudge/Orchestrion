86dbfcdfd459b680b5848d8c10b48306f28e19ff
Scott Mudge <mail@scottmudge.com>
2017-10-08 23:18:28 -0400

    Playing around with converting midi data into the 2-dimensional data.

--- critic/data_parser/data_parser.py
+++ critic/data_parser/data_parser.py
@@ -42,34 +42,86 @@ from mido import MidiFile
 
 class MidiReader:
     """This uses mido to read through midis in a list."""
-    midiPaths = []
-    midis = []
+    midi_paths = []
+    midi_data = []
+    midi_parser = mido.Parser()
 
     # Add test data
     def add_midi_to_file_list(self, filename):
         """Loads a file into the file list."""
-        self.midiPaths.append(filename)
+        self.midi_paths.append(filename)
 
-    def read_tracks(self):
-        """ This reads through the tracks in the midiPaths list and loads them into memory. """
-        for i, midi_fp in enumerate(self.midiPaths):
-            print("Reading MIDI file [" + str(i) + "]...")
-            self.midis.append(MidiFile(midi_fp))
+    def read_midis(self):
+        """ This reads through the tracks in the midiPaths list and loads them into memory.
+
+        @return Number of valid midis"""
+        # Local vars
+        valid_midi_count = 0
+
+        # Enumerate through the midi files
+        for i, midi_fp in enumerate(self.midi_paths):
+            print("Reading MIDI file [" + str(i) + "] (" + midi_fp + ")...")
+            midi_file = MidiFile(midi_fp)
             print("\t--> Loaded!")
-            track_count = len(self.midis[i].tracks)
+            track_count = len(midi_file.tracks)
             if track_count < 1:
                 print("No valid tracks found!")
-                break
             else:
                 print("\t--> " + str(track_count) + " total tracks:")
-                for i, track in enumerate(self.midis[i].tracks):
+                for i, track in enumerate(midi_file.tracks):
                    print('\t\t > Track {}: {}'.format(i, track.name))
+
+                # Read data
+                data = open(midi_fp, "rb").read()
+
+                # Get
+                self.midi_parser.feed(data)
+                total_messages = self.midi_parser.pending()
+                print("\t--> " + str(total_messages) + " total MIDI messages")
+                print("\t--> Reading messages...")
+
+                width = int(np.math.sqrt(total_messages))
+                width = width
+                height = width
+
+                out_img = np.ndarray((width, height, 3), np.int8)
+                out_img.fill(0)
+
+                msg = self.midi_parser.get_message()
+                x = 0
+                y = 0
+                while msg is not None and x < width and y < height:
+                    msg_d = msg.dict()
+                    # print(msg)
+                    chan = msg_d.get("channel", 0)
+                    note = msg_d.get("note", 0)
+                    vel = msg_d.get("velocity", 0)
+                    out_img[x][y][0] = np.int8(chan)
+                    out_img[x][y][1] = np.int8(note)
+                    out_img[x][y][2] = np.int8(vel)
+                    x += 1
+                    if x >= width:
+                        x = 0
+                        y += 1
+                    msg = self.midi_parser.get_message()
+
+               # img_min = out_img.min(axis=(0,1), keepdims=True)
+               # img_max = out_img.max(axis=(0,1), keepdims=True)
+               # out_img = (out_img - img_min)/(img_max-img_min)
+                cv.imwrite("C:/out" + str(i) + ".bmp", out_img)
+
+                valid_midi_count += 1
+
+                print("Done reading MIDI file [" + str(i) + "]")
+
+        return valid_midi_count
 
 
 if __name__ == "__main__":
     reader = MidiReader()
     reader.add_midi_to_file_list("../../data/midis/tf_in_d_minor.mid")
-    reader.read_tracks()
+    reader.add_midi_to_file_list("../../data/midis/bach7.mid")
+    reader.read_midis()
 
 # out_img = cv.imwrite("out_image.png")
 
